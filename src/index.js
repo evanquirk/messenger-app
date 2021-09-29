58d0e6af6802b4a289988785d798ea9776215c6f
Evan <evan_quirk@me.com>
2021-09-28 18:40:18 -0700

    fix: adjust index for React DOM and App import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { App } from './App';
 
-import App from './App'
-
-ReactDOM.render(<APP />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
