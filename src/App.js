fdab49e525cf3075301e70a5be2f127678b70ca8
Evan <evan_quirk@me.com>
2021-10-02 20:49:50 -0700

    feat: input variables for chat engine to render on site

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 
 import './App.css'
+
+export const App = () => {
+  const {
+    REACT_APP_PROJECT_ID,
+    REACT_APP_USERNAME,
+    REACT_APP_SECRET
+  } = process.env
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID={REACT_APP_PROJECT_ID}
+      userName={REACT_APP_USERNAME}
+      userSecret={REACT_APP_SECRET}
+    />
+  )
+}
