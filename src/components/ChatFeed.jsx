fbbcd7347b8cd0c24e4cc1c64f883121e8fd6bed
Evan Quirk <evan_quirk@me.com>
2021-10-04 17:12:09 -0700

    feat: add Chat Feed component logic

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,43 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+export default function ChatFeed(props) {
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1]
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className ="message-block">
+            {
+              isMyMessage
+              ? <MyMessage />
+              : <TheirMessage />
+            }
+          </div>
+          <div 
+            className="read-reciepts" 
+            style={{ 
+              marginRight: isMyMessage ? '18px' : '0px', 
+              marginLeft: isMyMessage ? '0px' : '68px' 
+            }}>
+            readReceipts
+          </div>
+
+        </div>
+      )
+    })
+  }
+
+  renderMessages()
+
+}
