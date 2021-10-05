6ca990560e3ad6abf89b20f523b6d73bb3483b59
Evan Quirk <evan_quirk@me.com>
2021-10-04 17:13:41 -0700

    feat: add renderMessages logic for ChatFeed component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -37,7 +37,23 @@ export default function ChatFeed(props) {
       )
     })
   }
-
-  renderMessages()
-
+  
+  if (!chat) {
+    return 'Loading...';
+  }
+  return (
+    <div classname="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+            {chat.people.map((person) => `${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  )
 }
