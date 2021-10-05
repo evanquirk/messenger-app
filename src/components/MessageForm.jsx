165834c8405bb833dc778866b11cff26a2b4931b
Evan Quirk <evan_quirk@me.com>
2021-10-04 21:12:21 -0700

    feat: add handleSubmit and handleChange logic to form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
-export default function MessageForm() {
+import { sendMessage, isTyping } from 'react-chat-engine';
+
+export default function MessageForm(props) {
   const [value, setValue] = useState('')
+  const { chatId, creds } = props;
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
+    const text = value.trim();
+
+    if (text.length > 0) sendMessage(creds, chatId, { text })
   }
 
-  const handleChange = () => {
+  const handleChange = (event) => {
+    setValue(event.target.value);
 
+    isTyping(props, chatId);
   }
   
   return (
