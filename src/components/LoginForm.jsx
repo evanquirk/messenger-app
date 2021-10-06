dcfe415df6ec71fd4a633a1259d064f4aa0f0e6a
Evan Quirk <evan_quirk@me.com>
2021-10-05 21:30:09 -0700

    chore: scaffold LoginForm component - inputs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,10 +5,37 @@ export default function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = userState('');
 
+  const handleSubmit = () => {
+
+
+  }
 
   return (
-    <div>
-      
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              value={username} 
+              onChange={(e)=> setUsername(e.target.value)} 
+              className="input" 
+              placeholder="Username" 
+              required/>
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e)=> setPassword(e.target.value)} 
+              className="input" 
+              placeholder="Password" 
+              required/>
+            <div align="center">
+              <button type="submit" className="button">
+                <span>Start Chatting</span>
+              </button>
+            </div>
+          </form>
+      </div>
     </div>
   )
 }
