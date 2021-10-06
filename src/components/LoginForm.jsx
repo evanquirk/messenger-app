f841bbd54ac3892c2c470bdcdd26ed76b388c0dc
Evan Quirk <evan_quirk@me.com>
2021-10-05 21:25:45 -0700

    chore: scaffold LoginForm component

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = userState('');
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
